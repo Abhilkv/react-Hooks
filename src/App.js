dcfd04e25ff98fe217965f7071909a385d1c29a6
abhil-perro <abhil@25holdings.com>
2020-03-28 00:19:15 +0530

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  // this useEffect will be executed after each render of the component 
+  // works for each charecter entered on input / for each button click
+  useEffect(()=>{
+    document.title = `CLICK: ${count}`
+  });
+
+  // bellow useEffect will be executed only when the specified value `count` is changed it won't execute after each render 
+  // works only for the butoon click
+  useEffect(()=>{
+    alert(count);
+  },[count]);
+
+  // this useEffect with empty dependancy array will we involked only once and mimics componentDidMount methode
+  useEffect(()=>{
+    alert('COMPONENT DID MOUNT IS CALLED');
+    return () => {
+      alert('COMPONENTWILLUNMOUNT IS INVOLKED');
+    }
+  }, [ ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <input type='text'></input>
+        <button onClick={() => {setCount( preValue => preValue + 1 )}} > 
+          You clicked on this {count} times
+        </button>
+      </div>
     </div>
   );
 }
