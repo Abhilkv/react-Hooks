fff871fc58836d0c5d245fd9f7d4330e3ffbf935
abhil-perro <abhil@25holdings.com>
2020-03-28 13:25:12 +0530

    useEffect with axios call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
+
 function App() {
+  const [ data, setDate] = useState([]);
+
+  useEffect( () => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then( res => {
+        setDate(res.data);
+      })
+      .catch( err => {
+        alert(err);
+      })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {data.map((item) => (
+       <li key={item.id}>
+         {item.title}
+         <br /><br />
+       </li>
+     ))}
     </div>
   );
 }
