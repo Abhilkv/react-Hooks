667b0d69c1d4d6271161e1fca547f26cc2061ccf
abhil-perro <abhil@25holdings.com>
2020-03-29 09:48:10 +0530

    useContext hook and provider, consumer methode

--- src/componentB.jsx
+++ src/componentB.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import {userContext} from './App';
+import { useContext } from 'react';
+// to get the context value from the provider we have 2 methodes 1, using .consumer or 
+// using useContext hook, using consumer is an old methode
+// value given to useContext will be a context object itself
 
 function B() {
+  const value = useContext(userContext);
   return (
      <div>
-          <userContext.consumer>
+        {/* <userContext.consumer>
             { 
                 arr => {
                     return <p> Name is {arr}</p>
                 } 
             }
-      </userContext.consumer>
+      </userContext.consumer> */}
+      <div>
+      name is  {value}
+      </div>
      </div>
   );
 }
