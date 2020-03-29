2fd767d45103d57f5fa159dc96d3eb2b54565e14
abhil-perro <abhil@25holdings.com>
2020-03-29 09:48:22 +0530

    useContext hook and provider, consumer methode

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import A from './componentA'
 import './App.css';
 
 export const userContext = React.createContext()
-
+// even if you use consumer command or useContext hook at the destination the provider is coded as the same as below
 function App() {
   return (
     <div>
