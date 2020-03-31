ae7de6394898c487605854eb6832eb8a06c39faa
abhil-perro <abhil@25holdings.com>
2020-03-31 10:18:45 +0530

    useRef

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef, useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const inputRef = useRef(null);
+  const timerRef = useRef(null);
+  const [time, setTime] = useState(0);
+
+
+// to focus to an input we can use useRef 
+// if we dont assign setInterval to a ref variable instead of a normal const variable it will not be accessable to call
+// clearInterval as the const variable have only local scope but here we can call clearInterval from the buttons onClick as well 
+  useEffect (()=> {
+    inputRef.current.focus();
+    timerRef.current = setInterval( () => {
+      setTime( prev => prev + 1)
+    }, 1000)
+    return () => {
+      clearInterval(timerRef.current)
+    }
+  }, []);
+
+  const setTimer = () => {
+    clearInterval(timerRef.current)
+    timerRef.current = setInterval( () => {
+      setTime( prev => prev + 1)
+    }, 1000);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display: 'flex'}}>
+      <input ref={inputRef}/>
+      {time}
+      <button onClick={()=> {clearInterval(timerRef.current)}}>stop time</button>
+      <button onClick={setTimer}>start time</button>
     </div>
   );
 }
