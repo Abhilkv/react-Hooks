e540aaed891d0538a3d22805dcfb713573dbc7f4
abhil-perro <abhil@25holdings.com>
2020-05-27 14:25:41 +0530

    burger menu button created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [click, setClick] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header onClick={() => {setClick(prev => !prev)}} className="container">
+        <div className= {!click ? "lines" : "cross1"}></div>
+        <div className={!click ? "lines_two" : "hidden"}></div>
+        <div className={!click ? "lines" : "cross2"}></div>
       </header>
     </div>
   );
